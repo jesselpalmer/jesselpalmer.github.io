f134fa330b75778b52dfae8b01b2773aa86355cf
Jesse Palmer <jesselpalmer@gmail.com>
2017-07-08 18:16:17 -0700

    feat: make minor updates

--- src/components/PageHeader/PageHeader.js
+++ src/components/PageHeader/PageHeader.js
@@ -8,9 +8,9 @@ const PageHeader = () => (
     </h1>
     <hr />
     <nav id='top-nav'>
-      <a href='#Writing'>Writing</a> |&nbsp;
+      <a href='#Books'>Books</a> |&nbsp;
       <a href='#Speaking'>Speaking</a> |&nbsp;
-      <a href='#Open Source'>Open Source</a> |&nbsp;
+      <a href='#Open Source Contributions'>Open Source Contributions</a> |&nbsp;
       <a href='#Social Networks'>Social Networks</a> |&nbsp;
       <a href='#Contact Me'>Contact Me</a>
     </nav>
